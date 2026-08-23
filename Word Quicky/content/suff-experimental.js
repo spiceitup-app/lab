1f70090c5ced1bcd8999bd4572ec953823bdfbce
spiceitup-app <appspiceitup@gmail.com>
2026-08-23 18:13:12 +0200

    Add files via upload

--- Word Quicky/content/suff-experimental.js
+++ Word Quicky/content/suff-experimental.js
@@ -11,7 +11,7 @@
     { value: 2, weight: 30 },
     { value: 4, weight: 20 }
   ]
-  => Anzeige: 1–4 Strafschlücke
+  => Anzeige: 1–4 Strafschlucke
 */
 window.SUFF_EXPERIMENTAL = {
   1: {
@@ -20,43 +20,32 @@ window.SUFF_EXPERIMENTAL = {
   },
   2: {
     sips: [
-      { value: 1, weight: 40 },
-      { value: 2, weight: 30 },
-      { value: 3, weight: 20 },
-      { value: 4, weight: 10 }
+      { value: 1, weight: 60 },
+      { value: 2, weight: 40 },
     ],
     shots: [{ value: 0, weight: 100 }]
   },
   3: {
     sips: [
       { value: 1, weight: 10 },
-      { value: 2, weight: 15 },
-      { value: 3, weight: 20 },
-      { value: 4, weight: 25 },
-      { value: 5, weight: 20 },
-      { value: 6, weight: 10 }
+      { value: 2, weight: 35 },
+      { value: 3, weight: 45 },
+      { value: 4, weight: 10 },
     ],
     shots: [
-      { value: 0, weight: 50 },
-      { value: 1, weight: 50 }
+      { value: 0, weight: 100 },
     ]
   },
   4: {
     sips: [
-      { value: 3, weight: 5 },
-      { value: 4, weight: 8 },
-      { value: 5, weight: 10 },
-      { value: 6, weight: 12 },
-      { value: 7, weight: 15 },
-      { value: 8, weight: 20 },
-      { value: 9, weight: 15 },
-      { value: 10, weight: 15 }
+      { value: 3, weight: 30 },
+      { value: 4, weight: 35 },
+      { value: 5, weight: 25 },
+      { value: 6, weight: 10 },
     ],
     shots: [
-      { value: 0, weight: 10 },
-      { value: 1, weight: 25 },
-      { value: 2, weight: 40 },
-      { value: 3, weight: 25 }
+      { value: 0, weight: 70 },
+      { value: 1, weight: 30 },
     ]
   }
 };
