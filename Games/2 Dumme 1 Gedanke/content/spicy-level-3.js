2a1a4ce11575d1b6a8cedc01d2f894566b8d3366
spiceitup-app <appspiceitup@gmail.com>
2026-08-24 21:06:59 +0200

    Add files via upload

--- Games/2 Dumme 1 Gedanke/content/spicy-level-3.js
+++ Games/2 Dumme 1 Gedanke/content/spicy-level-3.js
@@ -1,5 +1,11 @@
 // Zwei Dumme, ein Gedanke – Spicy Intensity 3
 // Einfach weitere Texte als neue Zeilen im Array ergänzen.
 window.SPICY_LEVEL_3 = [
-  "Beispiel für Spicy Intensity 3"
+  'Ein Fetisch',
+'Ein Gegenstand für Frauen zum masturbieren',
+'Ein Gegenstand für Männer zum masturbieren',
+'Ein Gegenstand, den man sich in eine Körperöffnung steckt',
+'Ein Ort für einen Quickie',
+'Etwas, das man beim Konsum von Pilzen sieht',
+'Etwas, das man in einem Sexshop kauft',
 ];
