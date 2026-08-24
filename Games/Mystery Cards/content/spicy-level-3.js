f05b2f300da6299f71220cf0cdc5daaabae851d9
spiceitup-app <appspiceitup@gmail.com>
2026-08-24 04:23:38 +0200

    Add files via upload

--- Games/Mystery Cards/content/spicy-level-3.js
+++ Games/Mystery Cards/content/spicy-level-3.js
@@ -1,5 +1,11 @@
 window.MYSTERY_SPICY_LEVEL_3 = [
-  "Was ist eine intime Frage, die du normalerweise nie beantworten würdest?",
-  "Welche Wahrheit über dein Datingleben würde hier am meisten überraschen?",
-  "Was ist dein geheimster Turn-on?"
+  {text:"Welche zwei Mitspieler würden gemeinsam die besten Pornos drehen?", beziehungskiller: 1, spiceCall: true},
+{text:"Welche zwei Mitspieler sollten gemeinsam Sex haben?", beziehungskiller: 2, spiceCall: true},
+{text:"Mit welchem Mitspieler könntest du dir am ehesten Sex vorstellen?", beziehungskiller: 2, spiceCall: true},
+{text:"Mit welchem Mitspieler würdest du am ehesten ein Porno drehen?", beziehungskiller: 2, spiceCall: true},
+{text:"Mit welchem Mitspieler würdest du am ehesten ein Dreier haben wollen?", beziehungskiller: 2, spiceCall: true},
+{text:"Welchen der Mitspieler würdest du am ehesten einmal beim Sex zuschauen?", beziehungskiller: 0, spiceCall: false},
+{text:"Welchem Mitspieler würdest du am ehesten ein Nude schicken?", beziehungskiller: 2, spiceCall: true},
+{text:"Mit welchem Mitspieler könntest du am ehesten Sex haben ohne Gefühle zu entwickeln?", beziehungskiller: 2, spiceCall: true},
+{text:"Welcher Mitspieler denkst du hat die heißesten Füße?", beziehungskiller: 2, spiceCall: true},
 ];
